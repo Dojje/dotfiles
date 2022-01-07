8c38bbcb0a6572098bad4bc9c24f30342050f200
dojje <50925050+dojje@users.noreply.github.com>
2022-01-08 00:01:21 +0100

    added i3lock-color

--- install.sh
+++ install.sh
@@ -1,44 +1,45 @@
 #!/bin/bash
 
+# Update the system
+sudo pacman -Syu
+
 read -p 'Install Packages? [y/n]: ' instpkg
 
 if [ $instpkg == "y" ]
 then
 
-# Update the system
-sudo pacman -Syu
-
 # download pacman packages
-sudo pacman -S htop
-sudo pacman -S xorg
-sudo pacman -S xorg-server
-sudo pacman -S xorg-xinit
-sudo pacman -S xf86-video-fbdev
-sudo pacman -S picom
-sudo pacman -S firefox
-sudo pacman -S get
-sudo pacman -S htop
-sudo pacman -S thunar
-
-sudo pacman -S rofi
-sudo pacman -S dmenu
-
-sudo pacman -S rsync
-sudo pacman -S ttf-ubuntu-font-family
-sudo pacman -S materia-gtk-theme
-sudo pacman -S papirus-icon-theme
-sudo pacman -S lxappearance
-sudo pacman -S i3
-sudo pacman -S i3wm
-sudo pacman -S i3lock
-sudo pacman -S i3status
-sudo pacman -S i3blocks
-sudo pacman -S reflector
-sudo pacman -S terminator
+sudo pacman -S htop \
+xorg \
+xorg-server \
+xorg-xinit \
+xf86-video-fbdev \
+picom \
+firefox \
+get \
+htop \
+thunar \
+rofi \
+dmenu \
+rsync \
+ttf-ubuntu-font-family \
+materia-gtk-theme \
+papirus-icon-theme \
+lxappearance \
+i3 \
+i3wm \
+i3lock \
+i3status \
+i3blocks \
+reflector \
+terminator \
 
 fi
-
+# it cannot use relector before installing relector
+echo "updating mirrorlist"
 sudo reflector -c Sweden -a 6 --sort rate --save /etc/pacman.d/mirrorlist
+echo "updating again"
+sudo pacman -Syu
 
 echo "This will copy any already existing dotfiles to .old-dotfiles directory"
 
@@ -85,8 +86,13 @@ ln -s ~/dotfiles/.config/rofi/config ~/.config/rofi/config
 
 # for rofi theme
 # it's unnecicerry to create extra folders because it's going into the same folder as rofi config
-ln -s ~/dotfiles/.config/rofi/devdeo_nord.rasi ~/.config/rofi/devdeo_nord.rasi
+ln -s ~/dotfiles/.config/rofi/config.rasi ~/.config/rofi/config.rasi
 
 # For xinitrc
 ln -s ~/dotfiles/.xinitrc ~/.xinitrc
+
+# For i3lock-color
+mkdir -p ~/.config/i3lock-color
+ln -s ~/dotfiles/.config/i3lock-color/lock.sh ~/.config/i3lock-color/lock.sh
+sudo chmod +x ~/dotfiles/.config/i3lock-color/lock.sh
 
